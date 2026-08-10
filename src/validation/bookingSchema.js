e3cc21540ca1d870806170555ade8debd563c204
patricenyuyseni <patriceflezy123@gmail.com>
2026-08-10 15:12:40 +0100

    updated README.md

--- src/validation/bookingSchema.js
+++ src/validation/bookingSchema.js
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 const createBookingSchema = z.object({
   customer_id: z.number().int().positive(),
-  quantity: z.number().int().positive(),
+  quantity: z.number().int().min(1).max(10),
 });
 
 export { createBookingSchema };
